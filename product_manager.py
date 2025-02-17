968198e3b21c8221dd105f8c05185a061d50d665
robertgen <robertstefan678@gmail.com>
2025-02-17 11:42:45 +0200

    Update product_manager.py

--- product_manager.py
+++ product_manager.py
@@ -1,20 +1,23 @@
+from product import Product
 class ProductManager:
-    products = [
-        ["Laptop", 1000, 50],
-        ["Keyboard", 400, 120],
-        ["Mouse", 150, 150],
-        ["Airpods", 430, 10],
-        ["Charges", 540, 87]
-]
+    def __init__(self):
+        self.products = [
+            Product("Laptop", 1000, 50),
+            Product("Keyboard", 400, 120),
+            Product("Mouse", 150, 150),
+            Product("Airpods", 430, 10),
+            Product("Charges", 540, 87)
+        ]
     def display_all_products(self):
-        for i in self.products:
-            print(f"Name: {i[0]}, Price: {i[1]}, Quantity: {i[2]}")
+        for product in self.products:
+            product.display_info()
     def total_value(self):
         total = 0
         for j in self.products:
             total += j[1] * j[2]
         return total
     def add_product(self, name, price, quantity):
-        self.products.append([name, price, quantity])
+        new_product = Product(name, price, quantity)
+        self.products.append(new_product)
                
     
